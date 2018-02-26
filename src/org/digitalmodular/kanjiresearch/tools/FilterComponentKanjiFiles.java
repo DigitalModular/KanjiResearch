58bddc085feb705d2e35a089cb3a8785ea12a791
Mark Jeronimus <mark.jeronimus@gmail.com>
2018-02-26 21:25:42 +0100

    Components filtered by kanji sets (and component frequencies)

---
+++ src/org/digitalmodular/kanjiresearch/tools/FilterComponentKanjiFiles.java
@@ -0,0 +1,120 @@
+/*
+ * This file is part of KanjiResearch.
+ *
+ * Copyleft 2018 Mark Jeronimus. All Rights Reversed.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllUtilities. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.digitalmodular.kanjiresearch.tools;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.digitalmodular.kanjiresearch.util.ComponentFileEntry;
+import org.digitalmodular.kanjiresearch.util.ComponentFileIO;
+import org.digitalmodular.kanjiresearch.util.KanjiList;
+import org.digitalmodular.kanjiresearch.util.KanjiSetFileIO;
+import org.digitalmodular.kanjiresearch.util.RadKFileIO;
+
+/**
+ * Filters the component list to only the kanji of each set.
+ * <p>
+ * Requires: <tt>radkfilex</tt>, <tt>*-set.utf8</tt>
+ * <p>
+ * Produces: <tt>*-components.utf8</tt>
+ *
+ * @author Mark Jeronimus
+ */
+// Created 2018-02-17
+public final class FilterComponentKanjiFiles {
+	private static final Comparator<ComponentFileEntry> COMPONENT_SORTER =
+			Comparator.comparingInt(ComponentFileEntry::size).reversed()
+			          .thenComparingInt(ComponentFileEntry::getRadical);
+
+	public static void main(String... args) throws IOException {
+		System.setProperty("line.separator", "\n");
+
+		String[] filenames = Files.list(Paths.get("kanjisets"))
+		                          .map(Path::toString)
+		                          .filter(filename -> filename.endsWith("-set.utf8"))
+		                          .toArray(String[]::new);
+
+		for (String filename : filenames)
+			process(filename);
+	}
+
+	private static void process(String filenameIn) throws IOException {
+		KanjiList                      kanjiInSet = KanjiSetFileIO.read(filenameIn);
+		Collection<ComponentFileEntry> components = RadKFileIO.read("componentsets/radkfilex", "EUC-JP");
+
+		components.forEach(component -> component.retainAll(kanjiInSet));
+
+		makeComponentFrequencyFile(filenameIn, components);
+		// This one last because it modifies 'components'.
+		makeComponentsFile(filenameIn, components);
+	}
+
+	private static void makeComponentsFile(String filenameIn, Collection<ComponentFileEntry> components)
+			throws IOException {
+		components.removeIf(KanjiList::isEmpty);
+
+		String filenameOut = makeFilename(filenameIn, "components-filtered-per-set", "-components.utf8");
+		ComponentFileIO.write(components, filenameOut);
+
+		System.out.println(filenameIn + " -> " + filenameOut);
+	}
+
+	private static void makeComponentFrequencyFile(String filenameIn, Collection<ComponentFileEntry> components)
+			throws IOException {
+		AtomicInteger index     = new AtomicInteger();
+		String        firstLine = "ComponentID\tComponent\tCount";
+		List<String> lines = Stream.concat(Stream.of(firstLine),
+		                                   components.stream()
+		                                             .map(entry -> toFrequencyString(entry, index)))
+		                           .collect(Collectors.toList());
+
+		String filenameOut = makeFilename(filenameIn, "component-frequencies-per-set", "-component-frequencies.tsv");
+		Files.write(Paths.get(filenameOut), lines);
+
+		System.out.println(filenameIn + " -> " + filenameOut);
+	}
+
+	private static String toFrequencyString(ComponentFileEntry entry, AtomicInteger index) {
+		return index.incrementAndGet() + "\t" +
+		       entry.getRadicalAsString() + '\t' +
+		       Integer.toString(entry.size());
+	}
+
+	private static String makeFilename(String filenameIn, String directory, CharSequence suffix) {
+		//noinspection DynamicRegexReplaceableByCompiledPattern // Suppress IntelliJ Bug (this is not a regex)
+		return filenameIn.replace("kanjisets", directory)
+		                 .replace("-set.utf8", suffix);
+	}
+}
---
+++ src/org/digitalmodular/kanjiresearch/util/ComponentFileIO.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of KanjiResearch.
+ *
+ * Copyleft 2018 Mark Jeronimus. All Rights Reversed.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllUtilities. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.digitalmodular.kanjiresearch.util;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Reader/writer for our component&rarr;kanji mapping (<tt>*-component-kanji.utf8</tt>) file format.
+ *
+ * @author Mark Jeronimus
+ */
+// Created 2018-02-15
+public final class ComponentFileIO {
+	private ComponentFileIO() { throw new AssertionError(); }
+
+	public static void write(Collection<ComponentFileEntry> components, String filename) throws IOException {
+		List<String> lines = components.stream()
+		                               .map(ComponentFileIO::toComponentString)
+		                               .collect(Collectors.toList());
+
+		Files.write(Paths.get(filename), lines, StandardCharsets.UTF_8);
+	}
+
+	public static List<ComponentFileEntry> read(String filename) throws IOException {
+		return Files.lines(Paths.get(filename), StandardCharsets.UTF_8)
+		            .map(ComponentFileIO::fromComponentString)
+		            .collect(Collectors.toList());
+	}
+
+	private static String toComponentString(ComponentFileEntry entry) {
+		return entry.getRadicalAsString() + '\t' + entry.getKanjiString();
+	}
+
+	private static ComponentFileEntry fromComponentString(String line) {
+		ComponentFileEntry entry = new ComponentFileEntry(line.codePointAt(0));
+
+		line.codePoints()
+		    .skip(2)
+		    .forEach(entry::add);
+
+		return entry;
+	}
+}
