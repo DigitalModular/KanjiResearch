aad3efa87ad69c39b59ff1c66f32fa4e876af75a
Mark Jeronimus <mark.jeronimus@gmail.com>
2018-02-27 18:44:00 +0100

    +PrintKanjiSetSizesMain

---
+++ src/org/digitalmodular/kanjiresearch/tools/PrintKanjiSetSizesMain.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of KanjiResearch.
+ *
+ * Copyleft 2018 Mark Jeronimus. All Rights Reversed.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllUtilities. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.digitalmodular.kanjiresearch.tools;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.digitalmodular.kanjiresearch.util.KanjiSetFileIO;
+
+/**
+ * @author Mark Jeronimus
+ */
+// Created 2018-02-16
+public final class PrintKanjiSetSizesMain {
+	public static void main(String... args) throws IOException {
+		Files.list(Paths.get("kanjisets"))
+		     .map(Path::toString)
+		     .filter(filename -> filename.endsWith("-set.utf8"))
+		     .forEach(PrintKanjiSetSizesMain::process);
+	}
+
+	private static void process(String filename) {
+		try {
+			long codePointCount = KanjiSetFileIO.read(filename).size();
+			System.out.println(filename + '\t' + codePointCount);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
